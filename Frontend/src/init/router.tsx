f4b9d98e88ad8f651517d38f3b64cdc47ffc687b
hbrcaninovic <hbrcaninov1@etf.unsa.ba>
2025-11-13 16:46:32 +0100

    Adding faculty-management to router

--- Frontend/src/init/router.tsx
+++ Frontend/src/init/router.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Route, Routes } from 'react-router';
 import { Home } from '../page/home/home.tsx';
 import { Page1 } from '../page/page1/page1.tsx';
+import { FacultyManagementPage } from '../page/faculty-management/FacultyManagementPage.tsx'
 
 export function Router(): React.ReactNode {
     return (
         <Routes>
             <Route path="/" element={ <Home /> } />
             <Route path="/page1" element={ <Page1 /> } />
+            <Route path="/faculty-management" element={<FacultyManagementPage />} />  {/* nova ruta */}
         </Routes>
     )
 }
