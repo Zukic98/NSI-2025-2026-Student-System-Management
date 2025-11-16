03a529ea1959810ff2a70e42bb89571532af001c
azunic3 <azunic3@etf.unsa.ba>
2025-11-16 20:24:56 +0100

    added table course in university modul

--- Modules/University/University.Infrastructure/Db/UniversityDbContext.cs
+++ Modules/University/University.Infrastructure/Db/UniversityDbContext.cs
@@ -1,8 +1,31 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using University.Core.Entities;
 
-namespace University.Infrastructure.Db
+
+public class UniversityDbContext : DbContext
 {
-    public class UniversityDbContext : DbContext
+    public UniversityDbContext(DbContextOptions<UniversityDbContext> options)
+        : base(options)
     {
+    }
+
+    public DbSet<Course> Courses { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Course>(builder =>
+        {
+            builder.ToTable("university_Course");
+
+            builder.HasKey(c => c.Id);
+
+            builder.Property(c => c.Name).IsRequired();
+            builder.Property(c => c.Code).IsRequired();
+            builder.Property(c => c.Ects).IsRequired();
+            builder.Property(c => c.AcademicYear).IsRequired();
+            builder.Property(c => c.Semester).IsRequired();
+            builder.Property(c => c.ProfessorId).IsRequired();
+            builder.Property(c => c.FacultyId).IsRequired();
+        });
     }
 }
--- Modules/University/University.Infrastructure/DependencyInjection/ServiceCollectionExtensions.cs
+++ Modules/University/University.Infrastructure/DependencyInjection/ServiceCollectionExtensions.cs
@@ -1,11 +1,16 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
 
 namespace University.Infrastructure
 {
     public static class ServiceCollectionExtensions
     {
-        public static IServiceCollection AddUniversityModule(this IServiceCollection services)
+        public static IServiceCollection AddUniversityModule(this IServiceCollection services, IConfiguration configuration)
         {
+            services.AddDbContext<UniversityDbContext>(options =>
+                options.UseNpgsql(configuration.GetConnectionString("UniversityConnection")));
+       
             return services;
         }
     }
