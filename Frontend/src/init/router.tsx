3c343de9a0901068598aa9fa52c55a799b09fbf4
Esma <ebajramovi3@etf.unsa.ba>
2025-12-04 06:56:10 +0100

    Implement student dashboard

--- Frontend/src/init/router.tsx
+++ Frontend/src/init/router.tsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import { Route, Routes } from 'react-router';
-import { Home } from '../page/home/home.tsx';
-import { Page1 } from '../page/page1/page1.tsx';
+import React from "react";
+import { Route, Routes } from "react-router";
+import { Page1 } from "../page/page1/page1.tsx";
 import TwoFASetupPage from "../page/identity/2FASetupPage";
+import DashboardPage from "../page/student dashboard/page.tsx";
 
 export function Router(): React.ReactNode {
-    return (
-        <Routes>
-            <Route path="/" element={ <Home /> } />
-            <Route path="/page1" element={<Page1 />} />
-            <Route path="/2fa/setup" element={<TwoFASetupPage />} />
-        </Routes>
-    )
+  return (
+    <Routes>
+      <Route path="/" element={<DashboardPage />} />
+      <Route path="/page1" element={<Page1 />} />
+      <Route path="/2fa/setup" element={<TwoFASetupPage />} />
+    </Routes>
+  );
 }
