a29f5bbaf2e8efdc4f956b481193014ff72b2110
Gabela2 <farisgabela1@gmail.com>
2025-12-29 19:35:32 +0100

    Removed unnecessary comments from files

--- Frontend/src/page/profile/ProfileSettings.tsx
+++ Frontend/src/page/profile/ProfileSettings.tsx
@@ -1,24 +1,3 @@
-/*Prije bilo
-import { CCard, CCardBody, CCardHeader } from '@coreui/react';
-import './ProfileSettings.css';
-
-export function ProfileSettings() {
-  return (
-    <div className="page-container">
-      <h1 className="page-title">Student profile settings</h1>
-      <CCard className="content-card">
-        <CCardHeader>
-          <strong>Profile Configuration</strong>
-        </CCardHeader>
-        <CCardBody>
-          <p>Profile settings and preferences will be available here.</p>
-        </CCardBody>
-      </CCard>
-    </div>
-  );
-}*/
-
-/*Moj kod*/
 import {
   CCard,
   CCardBody,
